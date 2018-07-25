a9355f595b12585b1ad8d2f23c83bc8ef029cd99
iron1 <iron1@CarlosPC>
2018-07-24 19:42:51 -0500

    se ha añadido el login (aun no esta completado y funcional)

---
+++ src/java/controlador/ManagedBeanLogin.java
@@ -0,0 +1,47 @@
+package controlador;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named(value = "managedBeanLogin")
+@RequestScoped
+public class ManagedBeanLogin {
+
+    private String user;
+    private String pass;
+
+    public String validarUsuario() {
+        if (user.equals("carlos") && pass.equals("hola")) {
+            System.out.println("HOla");
+            return "Administrador";
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, 
+                    "!Error¡", "No estás registrado en el sistema intentalo de nuevo"));
+            return null;
+        }
+    }
+
+    // Getters y setters
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    // Constructor por defecto
+    public ManagedBeanLogin() {
+    }
+
+}
