9d518280cd486b95358a6a060565ea61a5766c03
iron1 <iron1@CarlosPC>
2018-08-19 20:48:28 -0500

    Reestructuracion del proyecto y anexo del modulo de creacion de practicas

--- src/java/controlador/ManagedBeanLogin.java
+++ src/java/controlador/ManagedBeanLogin.java
@@ -1,9 +1,13 @@
 package controlador;
 
+import Entity.HibernateUtil;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 @Named(value = "managedBeanLogin")
 @RequestScoped
@@ -11,14 +15,22 @@ public class ManagedBeanLogin {
 
     private String user;
     private String pass;
-
+    private Session hibernateSession;
+    
     public String validarUsuario() {
-        if (user.equals("carlos") && pass.equals("hola")) {
+        hibernateSession = HibernateUtil.getSessionFactory().openSession(); 
+        Query query = hibernateSession.createQuery("from Usuarios where login = :user and passsword = :pass ");
+        query.setParameter("user", user);
+        query.setParameter("pass", pass);
+        
+        if ( !query.list().isEmpty() ) {
             System.out.println("HOla");
+            hibernateSession.close();
             return "docente";
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, 
                     "!Error¡", "No estás registrado en el sistema intentalo de nuevo"));
+            hibernateSession.close();
             return null;
         }
     }
--- src/java/controlador/ManagedBeanPractica.java
+++ src/java/controlador/ManagedBeanPractica.java
@@ -32,33 +32,37 @@ public class ManagedBeanPractica implements Serializable {
     public ManagedBeanPractica(){
     }
     
-    // Declaracion de variables
+    // Declaracion de variables ( 1er parte del modulo practica )
     private int numeropractica ;
     private String titulopractica;
-    private String competencias;
-    private String semestre;
-    private String creador;
-    private String editor;
+    private String autor;
     private String objetivos;
+    private String actividadprevia;
+    private String introduccion;
+
+    // Declaración de variables (3r parte del modulo practica)
+    private String desarrolloexp;
+    private String registrodatos;
+    private String resultados;
+    private String analiresul;
+    private String referencias;
+    private String nomenclatura;
+    private String anexos;
     
     public void guardar() throws JRException, IOException {        
         FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Éxito", " Datos guardados exitosamente ");
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
     
-    public String enviar(){
-        return "creacionPractica2";
-    }
-    
-    public String regresar(){
-        return "crearpractica1";
+    public String irA(String number){
+        return "creacionPractica"+ number;
     }
     
     public void printPDF() throws JRException, IOException{
         FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Éxito", " Datos guardados exitosamente ");
         FacesContext.getCurrentInstance().addMessage(null, msg);
         List<Practica> datasource = new ArrayList<>();
-        datasource.add(new Practica(numeropractica, titulopractica, competencias, semestre, creador, "Usuario actual", objetivos, ManagedBeanMaterial.list));
+        datasource.add(new Practica(numeropractica, titulopractica, "competencia", "semestre", "creador", "Usuario actual", objetivos, ManagedBeanMaterial.list));
         String filename = "Practica "+ numeropractica;
         String jasperPath = "/resources/FormatoDePractica.jasper";
         this.PDF(null, jasperPath, datasource, filename);
@@ -91,37 +95,13 @@ public class ManagedBeanPractica implements Serializable {
     public void setTitulopractica(String titulopractica) {
         this.titulopractica = titulopractica;
     }
-
-    public String getCompetencias() {
-        return competencias;
-    }
-
-    public void setCompetencias(String competencias) {
-        this.competencias = competencias;
-    }
-
-    public String getSemestre() {
-        return semestre;
-    }
-
-    public void setSemestre(String semestre) {
-        this.semestre = semestre;
-    }
-
-    public String getCreador() {
-        return creador;
-    }
-
-    public void setCreador(String creador) {
-        this.creador = creador;
-    }
-
+    
     public String getEditor() {
-        return editor;
+        return introduccion;
     }
 
-    public void setEditor(String editor) {
-        this.editor = editor;
+    public void setEditor(String introduccion) {
+        this.introduccion = introduccion;
     }
 
     public String getObjetivos() {
@@ -130,6 +110,88 @@ public class ManagedBeanPractica implements Serializable {
 
     public void setObjetivos(String objetivos) {
         this.objetivos = objetivos;
+    }
+    
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getActividadprevia() {
+        return actividadprevia;
+    }
+
+    public void setActividadprevia(String actividadprevia) {
+        this.actividadprevia = actividadprevia;
+    }
+
+    public String getIntroduccion() {
+        return introduccion;
+    }
+
+    
+    public void setIntroduccion(String introduccion) {
+        this.introduccion = introduccion;
+    }
+    
+    
+    public String getDesarrolloexp() {
+        return desarrolloexp;
+    }
+
+    public void setDesarrolloexp(String desarrolloexp) {
+        this.desarrolloexp = desarrolloexp;
+    }
+
+    public String getRegistrodatos() {
+        return registrodatos;
+    }
+
+    public void setRegistrodatos(String registrodatos) {
+        this.registrodatos = registrodatos;
+    }
+
+    public String getResultados() {
+        return resultados;
+    }
+
+    public void setResultados(String resultados) {
+        this.resultados = resultados;
+    }
+
+    public String getAnaliresul() {
+        return analiresul;
+    }
+
+    public void setAnaliresul(String analiresul) {
+        this.analiresul = analiresul;
+    }
+
+    public String getReferencias() {
+        return referencias;
+    }
+
+    public void setReferencias(String referencias) {
+        this.referencias = referencias;
+    }
+
+    public String getNomenclatura() {
+        return nomenclatura;
+    }
+
+    public void setNomenclatura(String nomenclatura) {
+        this.nomenclatura = nomenclatura;
+    }
+
+    public String getAnexos() {
+        return anexos;
+    }
+
+    public void setAnexos(String anexos) {
+        this.anexos = anexos;
     }
     
 }
