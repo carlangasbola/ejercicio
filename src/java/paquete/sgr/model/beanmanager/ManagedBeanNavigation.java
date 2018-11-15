1aed7a3bd53473df149830b2eec84f7f4b16ddde
iron1 <iron1@10.100.67.102>
2018-11-14 20:50:30 -0600

    Navegacion JSF

---
+++ src/java/paquete/sgr/model/beanmanager/ManagedBeanNavigation.java
@@ -0,0 +1,24 @@
+package paquete.sgr.model.beanmanager;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author iron1
+ */
+@Named(value = "navegar")
+@RequestScoped
+public class ManagedBeanNavigation {
+
+    /**
+     * Creates a new instance of ManagedBeanNavigation
+     */
+    public ManagedBeanNavigation() {
+    }
+    
+    public String irA(String pagina){
+        return pagina;
+    }
+    
+}
